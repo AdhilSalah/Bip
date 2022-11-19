1f76ca8f511bfda23373020fd2c2b8aad2c8e9ff
AdhilSalah <iamadhilsalah@gmail.com>
2022-11-19 16:12:38 +0530

    a

---
+++ parenthesys.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        opcl = dict(('()', '[]', '{}'))
+        print(opcl)
+
+        stack = []
+
+        for idx in s:
+         
+            if idx in '([{':
+                stack.append(idx)
+ 
+            elif len(stack) == 0 or idx != opcl[stack.pop()]:
+                return False
+
+        return len(stack) == 0
+
+
+print(Solution().isValid("()}"))
